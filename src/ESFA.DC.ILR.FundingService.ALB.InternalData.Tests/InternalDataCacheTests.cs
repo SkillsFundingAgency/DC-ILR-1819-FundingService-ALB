bb7f165df72f51a4e076e2b827ef7facb291845d
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-06-01 11:32:45 +0100

    51282 - Unit test refactoring

---
+++ src/ESFA.DC.ILR.FundingService.ALB.InternalData.Tests/InternalDataCacheTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace ESFA.DC.ILR.FundingService.ALB.InternalData.Tests
+{
+    public class InternalDataCacheTests
+    {
+    }
+}
---
+++ src/ESFA.DC.ILR.FundingService.ALB.TaskProvider.Tests/TaskProviderTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.FundingService.ALB.InternalData;
+using ESFA.DC.ILR.FundingService.ALB.InternalData.Interface;
+using ESFA.DC.ILR.FundingService.ALB.OrchestrationService;
+using ESFA.DC.ILR.FundingService.ALB.OrchestrationService.Interface;
+using ESFA.DC.ILR.FundingService.ALB.Service;
+using ESFA.DC.ILR.FundingService.ALB.Service.Interface;
+using ESFA.DC.ILR.FundingService.ALB.TaskProvider.Interface;
+using ESFA.DC.ILR.FundingService.ALB.TaskProvider.Service;
+using ESFA.DC.IO.Dictionary;
+using ESFA.DC.IO.Interfaces;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR.FundingService.ALB.TaskProvider.Tests
+{
+    public class TaskProviderTests
+    {
+        /// <summary>
+        /// Run TaskProvider
+        /// </summary>
+        [Fact(DisplayName = "TaskProvider - Exists"), Trait("Funding Service", "Unit")]
+        public void ProcessFunding_Exists()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+    }
+}
